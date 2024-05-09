8e1bfd59accb3239a74b08599fd1c7b7313a4b5b
Moritz Lange <moritz.langebuy@web.de>
2024-05-09 16:34:08 +0900

    new1

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Movie was created.'
+    else
+      @movies = Movie.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    if @bookmark.destroy
+      redirect_to list_path(@list), notice: 'Movie was removed from the list.', status: :see_other
+    else
+      redirect_to list_path(@list), alert: 'Failed to remove the movie from the list.', status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+puts "Clearing old data"
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Seeding movies"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+movies = JSON.parse(response)["results"]
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Movies seeded"
